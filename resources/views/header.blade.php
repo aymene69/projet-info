b77561c0e19661683e76125f75cd186c1798a84c
Aymene <belmegaming@gmail.com>
2023-03-24 10:50:52 +0000

    Preparation inscription

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,6 +1,42 @@
+<?php
+$user = false;
+?>
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
-
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
     </head>
 <body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Quizz</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link" href="">Accueil</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="">Règles</a>
+                    </li>
+                </ul>
+                <ul class="navbar-nav">
+                    @if ($user)
+                    <li class="nav-item">
+                        Bonjour, {{ $user->name }} <a class="nav-link" href="">Déconnexion</a>
+                    </li>
+                    @else
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{  url('/login') }}">Se connecter</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{  url('/register') }}">Inscription</a>
+                    </li>
+                    @endif
+                </ul>
+            </div>
+        </div>
+    </nav>
