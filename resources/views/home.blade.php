c93384ad8a7ee65ca0ff59fd4482f0e016ac6544
eren-nere <123940543+eren-nere@users.noreply.github.com>
2023-03-28 17:09:02 +0000

    modif eren header/footer/menu/logo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,8 @@
 @include('layouts.header')
-        <p>Salut</p>
+    <a class="titreQ tailleTitre" href="{{route('home')}}">Quizz</a>
+    <span>
+        <a class="titreQ liens-quizz" href="{{route('home')}}">Accueil</a>
+        <a class="titreQ liens-quizz" href="{{route('regles')}}">Règles</a>
+        <a class="titreQ liens-quizz" href="{{route('classement')}}">Classement</a>
+    </span>
 @include('layouts.footer')
--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,7 +1,8 @@
-        <footer class="text-center text-lg-start bg-light text-muted">
-            <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
+        <footer class="text-center text-lg-start text-muted footer" style="z-index:-1; margin-left:-60px">
+            <div class="text-center p-3">
                 © 2023 - Projet info par <b>Aymene (n° etu), Eren (n° etu), Lucas (n° etu), Lilian (n° etu), Daniel (n° etu)</b>
             </div>
         </footer>
     </body>
+    <script>var menuToggle = document.getElementById('menu-toggle');var menu = document.getElementById('menu');var menuClose = document.getElementById('menu-close');var body = document.getElementById('body');menuToggle.addEventListener('click', function() {  menu.classList.toggle('show');  body.classList.toggle('show');});menuClose.addEventListener('click', function() {    menu.classList.toggle('show');    body.classList.toggle('show');});function myFunction(x) {    x.classList.toggle("change");}</script>
 </html>
