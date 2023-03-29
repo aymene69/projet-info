f13670e5d64fc912194fe67088667451c410314b
Aymene <belmegaming@gmail.com>
2023-03-29 12:25:31 +0000

    correction modifs eren

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,8 +1,3 @@
 @include('layouts.header')
-    <a class="titreQ tailleTitre" href="{{route('home')}}">Quizz</a>
-    <span>
-        <a class="titreQ liens-quizz" href="{{route('home')}}">Accueil</a>
-        <a class="titreQ liens-quizz" href="{{route('regles')}}">Règles</a>
-        <a class="titreQ liens-quizz" href="{{route('classement')}}">Classement</a>
-    </span>
+
 @include('layouts.footer')
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -25,9 +25,14 @@ $user = auth()->user();
     </div>
 
     <div class="nav" id="nav">
-        <a href="#"> <img src="https://i.ibb.co/T0d76kQ/logo-AVCTXT.png" "  class="logoIMG"/> </a>
+        <a href="#"> <img src="https://i.ibb.co/T0d76kQ/logo-AVCTXT.png" class="logoIMG"/> </a>
     </div>
-
+    <a class="titreQ tailleTitre" href="{{route('home')}}">Quizz</a>
+    <span>
+        <a class="titreQ liens-quizz" href="{{route('home')}}">Accueil</a>
+        <a class="titreQ liens-quizz" href="{{route('regles')}}">Règles</a>
+        <a class="titreQ liens-quizz" href="{{route('classement')}}">Classement</a>
+    </span>
 
     <ul class="container-connex titreQ">
         @if ($user)
