ba4340a6099cb44a05ac59e35a3fc4c9a844a1f9
Aymene <belmegaming@gmail.com>
2023-03-29 14:04:19 +0000

    ajouté seismes

--- resources/views/catastrophes.blade.php
+++ resources/views/catastrophes.blade.php
@@ -1,3 +1,49 @@
+<?php
+$response = file_get_contents("https://www.seismicportal.eu/fdsnws/event/1/query?limit=10&format=json&minmag=3");
+$data = json_decode($response);
+?>
 @include('layouts.header')
-
+<div class="container-fluid">
+    <h1>Catastrophes naturelles</h1>
+    <h3>Sont affichées ici les dernières catastrophes naturelles</h3>
+    <div class="row align-items-start text-center">
+        <div class="col">
+            <h3>Séismes</h3>
+            @foreach($data->features as $seisme)
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title"><i class="fa-solid fa-triangle-exclamation"></i> Séisme vers {{$seisme->properties->flynn_region}}</h5>
+                    <table class="table table-striped">
+                        <tr>
+                            <td><i class="fa-solid fa-signal"></i> Magnitude</td>
+                            <td>{{$seisme->properties->mag}} sur l'échelle de Richter</td>
+                        </tr>
+                        <tr>
+                            <td><i class="fa-regular fa-solid fa-globe"></i> Profondeur</td>
+                            <td>{{$seisme->properties->depth}} km</td>
+                        </tr>
+                        <tr>
+                            <td><i class="fa-solid fa-location-dot"></i> Latitude</td>
+                            <td>{{$seisme->properties->lat}}</td>
+                        </tr>
+                        <tr>
+                            <td><i class="fa-solid fa-location-dot"></i> Longitude</td>
+                            <td>{{$seisme->properties->lon}}</td>
+                        </tr>
+                    </table>
+                    <a href="https://www.emsc-csem.org/Earthquake/earthquake.php?id={{$seisme->properties->source_id}}" class="btn btn-primary">Voir en détail</a>
+                </div>
+            </div>
+            <br>
+            @endforeach
+            <br>
+        </div>
+        <div class="col">
+            One of three columns
+        </div>
+        <div class="col">
+            One of three columns
+        </div>
+    </div>
+</div>
 @include('layouts.footer')
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -7,6 +7,7 @@ $user = auth()->user();
 
         <link href="{{ URL::asset('css/style.css'); }}" rel="stylesheet">
         <link href="/css/style.css" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
     </head>
