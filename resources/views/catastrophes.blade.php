10178657af080066ed625d6cd4824364d0033c68
Aymene <belmegaming@gmail.com>
2023-03-30 13:58:11 +0000

    Ajouts alertes météo

--- resources/views/catastrophes.blade.php
+++ resources/views/catastrophes.blade.php
@@ -1,9 +1,17 @@
 <?php
 $response = file_get_contents("https://www.seismicportal.eu/fdsnws/event/1/query?limit=10&format=json&minmag=3");
 $data = json_decode($response);
-$tsunami = file_get_contents("https://api.weather.gov/alerts/active?limit=50");
-$tsunami = json_decode($tsunami);
-dd($tsunami);
+$options = array(
+'http'=>array(
+    'method'=>"GET",
+    'header'=>"Accept-language: fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7\r\n" .
+            "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36\r\n" // i.e. An iPad
+)
+);
+
+$context = stream_context_create($options);
+$meteo = file_get_contents("https://api.weather.gov/alerts/active", false, $context);
+$meteo = json_decode($meteo);
 ?>
 @include('layouts.header')
 <div class="container-fluid">
@@ -42,37 +50,50 @@ dd($tsunami);
             <br>
         </div>
         <div class="col">
-        <h3>Dernière alerte tsunami</h3>
-            @foreach($data->features as $seisme)
+        <h3>Dernières alertes météo (US)</h3>
+            @php
+            $i=0;
+            @endphp
+            @foreach($meteo->features as $info)
+            @if($info->properties->severity=="Severe")
+            @if(substr($info->properties->effective, 0, 10)==date("Y-m-d"))
             <div class="card" style="z-index: -1;">
                 <div class="card-body">
-                    <h5 class="card-title"><i class="fa-solid fa-triangle-exclamation"></i> Alerte tsunami vers {{$seisme->properties->flynn_region}}</h5>
+                    <h5 class="card-title"><i class="fa-solid fa-triangle-exclamation"></i> Alerte vers {{$info->properties->areaDesc}}</h5>
                     <table class="table table-striped">
                         <tr>
-                            <td><i class="fa-solid fa-signal"></i> Magnitude</td>
-                            <td>{{$seisme->properties->mag}} sur l'échelle de Richter</td>
+                            <td><i class="fa-solid fa-arrow-right"></i> Type d'alerte</td>
+                            <td>{{$info->properties->event}}</td>
                         </tr>
                         <tr>
-                            <td><i class="fa-regular fa-solid fa-globe"></i> Profondeur</td>
-                            <td>{{$seisme->properties->depth}} km</td>
+                            <td><i class="fa-solid fa-signal"></i> Gravité</td>
+                            <td>{{$info->properties->severity}}</td>
                         </tr>
                         <tr>
-                            <td><i class="fa-solid fa-location-dot"></i> Latitude</td>
-                            <td>{{$seisme->properties->lat}}</td>
+                            <td><i class="fa-sharp fa-solid fa-clover"></i> Certitude</td>
+                            <td>{{$info->properties->certainty}}</td>
                         </tr>
                         <tr>
-                            <td><i class="fa-solid fa-location-dot"></i> Longitude</td>
-                            <td>{{$seisme->properties->lon}}</td>
+                            <td><i class="fa-solid fa-calendar-days"></i> Début</td>
+                            <td>{{substr($info->properties->effective, 0, 10)}} à {{substr($info->properties->effective, 11, -9)}}</td>
+                        </tr>
+                        <tr>
+                            <td><i class="fa-solid fa-calendar-days"></i> Fin</td>
+                            <td>{{substr($info->properties->expires, 0, 10)}} à {{substr($info->properties->expires, 11, -9)}}</td>
                         </tr>
                     </table>
-                    <a href="https://www.emsc-csem.org/Earthquake/earthquake.php?id={{$seisme->properties->source_id}}" class="btn btn-primary">Voir en détail</a>
                 </div>
             </div>
             <br>
+            @endif
+            @endif
+            @php
+            $i++;
+            @endphp
+            @if($i==50)
+            @break
+            @endif
             @endforeach
-        </div>
-        <div class="col">
-            One of three columns
         </div>
     </div>
 </div>
