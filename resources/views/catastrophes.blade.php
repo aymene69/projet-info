ac0047bbac08d0684635a0e5ee60707e19563ab7
Aymene <belmegaming@gmail.com>
2023-03-30 13:12:24 +0000

    test

--- resources/views/catastrophes.blade.php
+++ resources/views/catastrophes.blade.php
@@ -1,6 +1,9 @@
 <?php
 $response = file_get_contents("https://www.seismicportal.eu/fdsnws/event/1/query?limit=10&format=json&minmag=3");
 $data = json_decode($response);
+$tsunami = file_get_contents("https://www.tsunami.gov/events/xml/PAAQAtom.xml");
+$tsunami = simplexml_load_string($tsunami);
+dd($tsunami);
 ?>
 @include('layouts.header')
 <div class="container-fluid">
@@ -39,7 +42,34 @@ $data = json_decode($response);
             <br>
         </div>
         <div class="col">
-            One of three columns
+        <h3>Dernière alerte tsunami</h3>
+            @foreach($data->features as $seisme)
+            <div class="card" style="z-index: -1;">
+                <div class="card-body">
+                    <h5 class="card-title"><i class="fa-solid fa-triangle-exclamation"></i> Alerte tsunami vers {{$seisme->properties->flynn_region}}</h5>
+                    <table class="table table-striped">
+                        <tr>
+                            <td><i class="fa-solid fa-signal"></i> Magnitude</td>
+                            <td>{{$seisme->properties->mag}} sur l'échelle de Richter</td>
+                        </tr>
+                        <tr>
+                            <td><i class="fa-regular fa-solid fa-globe"></i> Profondeur</td>
+                            <td>{{$seisme->properties->depth}} km</td>
+                        </tr>
+                        <tr>
+                            <td><i class="fa-solid fa-location-dot"></i> Latitude</td>
+                            <td>{{$seisme->properties->lat}}</td>
+                        </tr>
+                        <tr>
+                            <td><i class="fa-solid fa-location-dot"></i> Longitude</td>
+                            <td>{{$seisme->properties->lon}}</td>
+                        </tr>
+                    </table>
+                    <a href="https://www.emsc-csem.org/Earthquake/earthquake.php?id={{$seisme->properties->source_id}}" class="btn btn-primary">Voir en détail</a>
+                </div>
+            </div>
+            <br>
+            @endforeach
         </div>
         <div class="col">
             One of three columns
